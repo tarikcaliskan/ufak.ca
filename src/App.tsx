9937e52817041e58f1449b8949762df5d1875924
Alperen Karagüzel <55390822+alperenkaraguzel@users.noreply.github.com>
2020-07-03 19:25:46 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    Hello ufak.ca is hereeeeeee ! coming sooon
+    </div>
+  );
+}
+
+export default App;
