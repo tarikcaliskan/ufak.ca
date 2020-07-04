0321655d1b3c20110abefe15750608ec8186a92c
alperenkaraguzel <alperenkaraguzel@gmail.com>
2020-07-04 03:43:52 +0300

    favicon added and meta changed

---
+++ src/components/main/Modals/signIn.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import '../../App.css';
+function InputURL() {
+    const [value, setValue] = useState("")
+
+    return (
+        <div className="sign">
+        </div>
+    );
+}
+
+export default InputURL;
